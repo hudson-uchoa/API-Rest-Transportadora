49b5f1b8031991a4d771d4b2289a99304120fa67
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-04-24 23:06:52 -0300

    mudando porta

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{
-    console.log(`Servidor aberto na http://localhost:${port}/`)
-})
+app.listen(port, () => {
+  console.log(`Servidor aberto na http://localhost:${port}/`);
+});
