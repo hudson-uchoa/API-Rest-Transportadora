daa82fc57abd23d1534775f8810f66d804b0ca56
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-04-24 22:49:25 -0300

    modificando db

--- src/database/create-and-populate.js
+++ src/database/create-and-populate.js
@@ -23,14 +23,7 @@ INSERT INTO CLIENTES (ID, NOME_COMPLETO, CPF, TELEFONE, SENHA, EMAIL)
 VALUES 
     (1, 'Carlos Alberto Albuquerque', '44213242190', '27969216379', '@12345679' , 'carlos.alb12@gmail.com'),
     (2, 'Olívia Ribeiro Ferreira', '91975838041', '35998195626', '@12345679' , 'olivia.rib1@outlook.com'),
-    (3, 'Luiz Rodrigo Lima Mendes', '57014585045', '67988644718', '@12345679', 'luiz.rodrigo223@gmail.com'),
-    (4, 'Mirtes Faria Lima', '37875791090', '83985817934', '@12345679', 'm.faria.l1@gmail.com'),
-    (5, 'Rafael Monarca da Silva', '45655175070', '63991516460', '@12345679', 'rafa.monarca213@yahoo.com'),
-    (6, 'Ana Carolina dos Santos Faria', '80690964030', '17981481645', '@12345679', 'carol.ana14@gmail.com'),
-    (7, 'Lucas Rafael de Jesus', '25154112075', '96981677764', '@12345679', 'luca.rafa2@hotmail.com'),
-    (8, 'Erika Ferreira Luz', '93735415016', '84987453778', '@12345679', 'erika.ferr221@gmail.com'),
-    (9, 'Iago Malta Piteira', '19940979061', '47994722576', '@12345679', 'iago.malta22@outlook.com'),
-    (10, 'Márcia Melo Resende', '94137767092', '67987741537', '@12345679', 'marcia.resende.melo@gmail.com')
+    (3, 'Luiz Rodrigo Lima Mendes', '57014585045', '67988644718', '@12345679', 'luiz.rodrigo223@gmail.com')
 `;
 
 function criaTabelaClt() {
